863e4a8308a1fff5d776863d805dd8819a26ee21
Fernando Reinert <reinertpt@gmail.com>
2023-07-11 21:06:08 -0300

    projeto-organo

---
+++ src/componetes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+        <img src= "/imagens/banner.png" alt="Banner Principal da página Organo"/>
+        </header>
+    )
+}
+
+export default Banner
+
